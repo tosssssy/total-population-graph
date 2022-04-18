630e86ed863620e3b88b02874149941fe0f702d1
t-kabu <t.kabu.03@gmail.com>
2022-04-18 18:12:56 +0900

    feat(cache): キャッシュがあればそれを使用するように

--- src/hooks/usePrefecture.ts
+++ src/hooks/usePrefecture.ts
@@ -4,23 +4,32 @@ import { getApi } from 'utils/getApi'
 
 export const usePrefecture = (prefs: PrefInfo[]) => {
   const [selectedPrefs, setSelectedPrefs] = useState<PrefDetail[]>([])
+  const [cache, setCache] = useState<PrefDetail[]>([])
 
   const addPref = useCallback(
     async (prefCode: number) => {
-      const newData = await getApi<PrefDetailResponse>(
-        `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`
-      )
-      setSelectedPrefs((postData) => [
-        ...postData,
-        {
+      const cachedData = cache.find((v) => v.prefCode === prefCode)
+
+      let newData: PrefDetail
+
+      if (cachedData) {
+        newData = cachedData
+      } else {
+        const response = await getApi<PrefDetailResponse>(
+          `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`
+        )
+        newData = {
           prefCode,
           prefName: prefs.find((v) => v.prefCode === prefCode)?.prefName || '',
           data:
-            newData.result.data.find((v) => v.label === '総人口')?.data || [],
-        },
-      ])
+            response.result.data.find((v) => v.label === '総人口')?.data || [],
+        }
+        setCache((postData) => [...postData, newData])
+      }
+
+      setSelectedPrefs((postData) => [...postData, newData])
     },
-    [prefs]
+    [cache, prefs]
   )
 
   const deletePref = useCallback((prefCode: number) => {
