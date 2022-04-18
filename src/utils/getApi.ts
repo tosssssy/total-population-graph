f906c7f5b5cb1bdeee6c912a8654ebbd523e5ca3
t-kabu <t.kabu.03@gmail.com>
2022-04-18 18:41:02 +0900

    feat(getApi): エラーハンドリング処理を実装

--- src/utils/getApi.ts
+++ src/utils/getApi.ts
@@ -1,10 +1,13 @@
 export const getApi = async <T>(url: string) => {
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY || '',
-    },
-  })
-
-  return (await response.json()) as T
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': process.env.NEXT_PUBLIC_RESAS_API_KEY || '',
+      },
+    })
+    return (await response.json()) as T
+  } catch (error) {
+    console.error(error)
+  }
 }
